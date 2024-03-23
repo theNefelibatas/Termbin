b8b13fe8ffed3600c1d81ed5bf0266e5ef50d23a
Nepenthe <3368357411@qq.com>
2024-03-23 23:09:17 +0800

    feat: add id generator

---
+++ util/generator.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"math/rand"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func GenDigest(content string) (string, error) {
+	digest := sha256.Sum256([]byte(content))
+	digestStr := hex.EncodeToString(digest[:])
+	return digestStr, nil
+}
+
+func GenShortID(length int) (string, error) {
+	var stringBuilder = strings.Builder{}
+	for i := 0; i < length; i++ {
+		index := rand.Intn(len(alphabet))
+		stringBuilder.WriteByte(alphabet[index])
+	}
+	return stringBuilder.String(), nil
+}
+
+func GenUUID() (string, error) {
+	u, err := uuid.NewRandom()
+	return u.String(), err
+}
